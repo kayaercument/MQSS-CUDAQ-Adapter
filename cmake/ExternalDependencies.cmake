2cbf037b7118e330a9b0ffc69851cbe3bdc12b5e
Martin Letras <di35hec@badwlrz-cl14642>
2025-07-25 12:30:07 +0000

    📝 Updating documentation, including LICENSE and missing comments

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -5,7 +5,7 @@
 # "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# TODO: LICENSE
+# https://github.com/Munich-Quantum-Software-Stack/MQSS-CUDAQ-Adapter/blob/develop/LICENSE
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
