be7a7dd0862a211b0ef779f1b889d92272646ae3
Ercüment Kaya <kayaercument95@gmail.com>
2026-09-24 14:52:57 +0200

    🎨 update the dependency

--- cmake/Findmqss-client.cmake
+++ cmake/Findmqss-client.cmake
@@ -21,7 +21,7 @@ set(BUILD_SHARED_LIBS OFF)
 
 FetchContent_Declare(
   mqss-client
-  GIT_REPOSITORY https://github.com/kayaercument/MQSS-Client.git
+  GIT_REPOSITORY https://github.com/Munich-Quantum-Software-Stack/MQSS-Client.git
   GIT_TAG develop)
 
 FetchContent_MakeAvailable(mqss-client)
